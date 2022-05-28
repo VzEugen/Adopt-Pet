f9749f9f9be70372dc29f8c471eb012fd5ce2b52
Dumitru <pynzaru.dima@mail.ru>
2022-05-28 15:07:18 +0300

    generator3

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 const generator = (name, img, description, button) =>{
     const container = document.createElement("div");
     container.classList.add("main__card");
-    const img = document.createElement("img");
-    img.classList.add("m__card-image");
-    const name = document.createElement("h3")
+    const images = document.createElement("img");
+    img.classist.add("m__card-image");
+    const names = document.createElement("h3")
     name.classList.add("m__card-name")
-    const description = document.createElement("span")
+    const descriptions = document.createElement("span")
     description.classList.add("m__card-description")
-    const button = document.createElement("button")
+    const buttons = document.createElement("button")
     button.classList.add("m__card-submit")
       
     container.append(img);
